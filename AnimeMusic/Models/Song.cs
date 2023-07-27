a4a7e6e1d694c24935376f920ec864db38a47e88
Michael A. Momo <mayesamomo1997@gmail.com>
2023-07-27 15:28:38 -0400

    models added

---
+++ AnimeMusic/Models/Song.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AnimeMusic.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public string title{ get; set; }
+
+        
+    }
+}
